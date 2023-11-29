379e0223887293170b4dc564fbbab673abe38244
Varun <varun@tribalarena.com>
2023-11-29 11:16:50 +0530

    changes

--- neuralchat/core/azure.py
+++ neuralchat/core/azure.py
@@ -16,6 +16,53 @@ from core.User import User
 
 container_name = st.secrets["userContainer"]
 
+ALLOWED_EXTENSIONS = [
+    'png', 'jpg', 'jpeg', 'gif',  # Images
+    'txt', 'pdf',  # Text and PDFs
+    'doc', 'docx',  # Microsoft Word
+    'xls', 'xlsx',  # Microsoft Excel
+    # Add any other file extensions as needed
+]
+
+st.markdown("""
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <style>
+            .stMarkdown .list_wrapper{
+            display:flex;
+            align-items:center;
+            gap:12px
+            }
+            .list_style{
+            display:flex;
+            flex-direction:column;
+            }
+            .list_item_size{
+            color:#818181;
+            }
+        .file-row {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 10px;
+            background-color: #f0f0f0;
+            border-radius: 10px;
+            margin-bottom: 5px;
+        }
+        .file-name {
+            flex-grow: 1;
+            display: flex;
+            align-items: center;
+        }
+        .file-name span {
+            margin-right: 10px;
+        }
+        .material-icons {
+            color: #4caf50; /* Change color as needed */
+        }
+        /* Add more custom styles as needed */
+    </style>
+    """, unsafe_allow_html=True)
+
 
 def CreateContainer(user: User):
     try:
@@ -63,8 +110,12 @@ def loadBlobs(user: User):
     container_client = blob_service_client.get_container_client(container_name)
     blobs = container_client.list_blobs(name_starts_with=user.id.lower() + "/")
     df = pd.DataFrame([{'name': blob.name.removeprefix(user.id.lower() + "/"),
-                        'size': humanize.naturalsize(blob.size)} for blob in blobs])
-    st.dataframe(df)
+                        'size': humanize.naturalsize(blob.size), 'type': get_file_type(blob.name)} for blob in blobs])
+    for index, row in df.iterrows():
+        # Make sure that 'type' is the correct column name and it exists in your DataFrame
+        icon_class = get_file_icon(row['name'])
+        st.markdown(
+            f'''<div class="list_wrapper"><span class='material-icons'>{icon_class}</span> <div class="list_style"><span>{row['name']}</span> <span class="list_item_size">{row['size']}</span></div></div><hr/> ''', unsafe_allow_html=True)
 
 
 def main():
@@ -81,7 +132,7 @@ def main():
             # config = ConfigHelper.get_active_config_or_default()
             # file_type = [processor.document_type for processor in config.document_processors]
             uploaded_files = st.file_uploader("Upload Files",
-                                              # type=file_type,
+                                              type=ALLOWED_EXTENSIONS,
                                               accept_multiple_files=True)
             if uploaded_files is not None:
                 for up in uploaded_files:
@@ -112,3 +163,22 @@ def main():
 
     except Exception as e:
         st.error(traceback.format_exc())
+
+
+def get_file_type(blob_name):
+    mime_type, _ = mimetypes.guess_type(blob_name)
+    return mime_type if mime_type is not None else 'Unknown'
+
+
+def get_file_icon(blob_name):
+    mime_type, _ = mimetypes.guess_type(blob_name)
+    mime_to_icon = {
+        'image/jpeg': 'image',
+        'image/png': 'image',
+        'image/gif': 'image',
+        'text/plain': 'article',
+        'application/pdf': 'picture_as_pdf',
+        # ... other MIME types
+        'default': 'insert_drive_file'
+    }
+    return mime_to_icon.get(mime_type, mime_to_icon['image/png'])
